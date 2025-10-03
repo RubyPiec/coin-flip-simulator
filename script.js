973ea164700cdd9c6749c174439dae30c311b8c5
RubyPiec <50266387+RubyPiec@users.noreply.github.com>
2025-10-03 23:38:28 +0200

    i really really tried to add custom
    what have i done

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ let sideCount = 0
 let recentFlips = []
 let recentflipcount;
 
+let customsides = ["Heads", "Tails", "Side", "4", "5", "6", "7", "8", "9", "10"]
+let sideweights = [4999, 4999, 1, 1, 1, 1, 1, 1, 1, 1]
+let totalweight = 0;
+
 function cpl(count){ //check plural
     return count == 1 ? '' : 's'
 }
 
 function flip(){
+    updatechances()
     if(recentflipcount>1000000){
         if(!confirm("The amount of coins you'd like to flip is looking awfully high, and may be very laggy... are you sure you still want to do this?")){
             return;
@@ -25,8 +30,10 @@ function flip(){
         randnum = Math.random()
         if(document.getElementById("ideal").checked){
             idealFlip()
-        } else{
+        } else if(document.getElementById("realistic").checked){
             realisticFlip()
+        } else{
+            customFlip()
         }
     }
     update()
@@ -34,48 +41,90 @@ function flip(){
 
 function update(){
     document.getElementById("flipamount").value=document.getElementById("flipamount").value.replace(/\D/g, '')
-    if(flips>0){
-        if(recentflipcount==1){
-            let coindir = recentFlips[0]
-            if(randnum>=1/6000){
-                document.getElementById("coindirection").innerHTML = `Your coin landed on ${coindir}`
-            } else{
-                document.getElementById("coindirection").innerHTML = "Your coin landed... on its side? Impressive."
-            }
-        } else{
-            let rfH = recentFlips.filter(x => x=='Heads').length
-            let rfT = recentFlips.filter(x => x=='Tails').length
-            let rfs = recentFlips.filter(x => x=='side').length
-            let newstr = "Your coin landed on "
-            if(rfH){
-                newstr+=`Heads ${rfH} time${cpl(rfH)}`
-                if(rfT&&!rfs){
-                    newstr+=` and Tails ${rfT} time${cpl(rfT)}`
-                } else if(rfs&&!rfT){
-                    newstr+=` and on its side ${rfs} time${cpl(rfs)}`
-                } else if (rfs && rfT){
-                    newstr+=`, Tails ${rfT} time${cpl(rfT)}, and on its side ${rfs} time${cpl(rfs)}`
+    if(!document.getElementById("custom").checked){
+        document.getElementById("customsettings").classList.add("custom")
+        if(flips>0){
+            if(recentflipcount==1){
+                let coindir = recentFlips[0]
+                if(randnum>=1/6000){
+                    document.getElementById("coindirection").innerHTML = `Your coin landed on ${coindir}`
+                } else{
+                    document.getElementById("coindirection").innerHTML = "Your coin landed... on its side? Impressive."
                 }
             } else{
-                if(rfT){
-                    newstr+=`Tails ${rfT} time${cpl(rfT)}`
-                    if(rfs){
+                let rfH = recentFlips.filter(x => x=='Heads').length
+                let rfT = recentFlips.filter(x => x=='Tails').length
+                let rfs = recentFlips.filter(x => x=='side').length
+                let newstr = "Your coin landed on "
+                if(rfH){
+                    newstr+=`Heads ${rfH} time${cpl(rfH)}`
+                    if(rfT&&!rfs){
+                        newstr+=` and Tails ${rfT} time${cpl(rfT)}`
+                    } else if(rfs&&!rfT){
                         newstr+=` and on its side ${rfs} time${cpl(rfs)}`
+                    } else if (rfs && rfT){
+                        newstr+=`, Tails ${rfT} time${cpl(rfT)}, and on its side ${rfs} time${cpl(rfs)}`
                     }
                 } else{
-                    if(rfs){
-                        newstr+=`its side ${rfs} time${cpl(rfs)}`
+                    if(rfT){
+                        newstr+=`Tails ${rfT} time${cpl(rfT)}`
+                        if(rfs){
+                            newstr+=` and on its side ${rfs} time${cpl(rfs)}`
+                        }
+                    } else{
+                        if(rfs){
+                            newstr+=`its side ${rfs} time${cpl(rfs)}`
+                        }
                     }
-                }
-            }  
-            // document.getElementById("coindirection").innerHTML = `Your coin landed on Heads ${rfH} times, Tails ${rfT} times, and on its side ${rfs} times.`
-            document.getElementById("coindirection").innerHTML = newstr
-        }
-        document.getElementById("flips").innerHTML = `This is your ${numToOrdinal(flips)} flip.`
+                }  
+                // document.getElementById("coindirection").innerHTML = `Your coin landed on Heads ${rfH} times, Tails ${rfT} times, and on its side ${rfs} times.`
+                document.getElementById("coindirection").innerHTML = newstr
+            }
+            document.getElementById("flips").innerHTML = `This is your ${numToOrdinal(flips)} flip.`
 
-        document.getElementById("counts").innerHTML = `You have flipped Heads ${headCount} time${cpl(headCount)}.\nYou have flipped Tails ${tailCount} time${cpl(tailCount)}. ${sideCount > 0 ? 'Your coin has landed on its side ' + sideCount + ' time' + cpl(sideCount) + '.' : ''}\n\n${Math.round(100*headCount/flips)}% Heads\n${Math.round(100*tailCount/flips)}% Tails`
+            document.getElementById("counts").innerHTML = `You have flipped Heads ${headCount} time${cpl(headCount)}.\nYou have flipped Tails ${tailCount} time${cpl(tailCount)}. ${sideCount > 0 ? 'Your coin has landed on its side ' + sideCount + ' time' + cpl(sideCount) + '.' : ''}\n\n${Math.round(100*headCount/flips)}% Heads\n${Math.round(100*tailCount/flips)}% Tails`
+        }
+    } else{
+        document.getElementById("customsettings").classList.remove("custom")
+        totalweight=0
+
+        for(i=0;i<Number(document.getElementById("csides").value);i++){
+            sideweights[i]=Number(document.getElementById("weight"+i).value)
+            totalweight+=sideweights[i]
+        }
     }
     //i am so sorry for this code i promise i would never do this if i was working with someone else. please hire me future employers thank you
+}
+
+function updatechances(){
+    let csides = document.getElementById("csides").value
+    document.getElementById("csides").value=csides.replace(/\D/g, '')
+    csides = document.getElementById("csides").value
+    document.getElementById("csides").value=Math.max(Math.min(csides,10),1) // traps number between 1 and 10
+    csides = Number(document.getElementById("csides").value)
+
+    let chancelist = ""
+    for(i=1;i<=csides;i++){
+        chancelist+=`
+        <span>
+            <input id="name${i-1}" style="width:20%;" value="${customsides[i-1]}" oninput="updatechance(${i-1},0)">
+            <label>Name&nbsp;&nbsp;&nbsp;Weight</label>
+            <input id="weight${i-1}" style="width:20%;" value="${sideweights[i-1]}" oninput="updatechance(${i-1},1)">
+        </span>
+        `
+    }
+    document.getElementById("chances").innerHTML = chancelist
+}
+
+function updatechance(chance,type){ //0 is name, 1 is weight
+    document.getElementById("weight"+chance).value = document.getElementById("weight"+chance).value.replace(/\D/g, '')
+
+    if(type==0){
+        customsides[chance]=document.getElementById("name"+chance).value
+    } else{
+        sideweights[chance]=Number(document.getElementById("weight"+chance).value)
+    }
+    update()
 }
 
 let randnum = Math.random() //global variable plz
@@ -99,6 +148,7 @@ function realisticFlip(){
         }
     }
 }
+// sources for both comments in this function are in the wikipedia page https://en.wikipedia.org/wiki/Coin_flipping
 
 function idealFlip(){
     if(randnum<0.5){
@@ -108,6 +158,9 @@ function idealFlip(){
         recentFlips.push('Tails')
         tailCount++
     }
+}
+
+function customFlip(){
 }
 
 function numToOrdinal(num){
@@ -135,4 +188,4 @@ function togglemenu(){
     }
 }
 
-// sources for all comments are in the wikipedia page https://en.wikipedia.org/wiki/Coin_flipping
+window.onload = function(){update();}
